4fc627d648fd71362a616f92c09b0bde12c506de
kaveeshapraveen99 <kaveeshapraveen99@gmail.com>
2023-04-01 10:23:05 +0530

    Created user profile and clarification pages.

---
+++ app/src/main/java/com/example/greencare/UserProfilePage.java
@@ -0,0 +1,14 @@
+package com.example.greencare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_page);
+    }
+}
